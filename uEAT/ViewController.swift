808ee82490ff9d503fabeba7ba77fa7cb90d8fd5
Cracc team <31965340+Craccteam@users.noreply.github.com>
2019-10-18 18:34:17 -0400

    Initial Commit

---
+++ uEAT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uEAT
+//
+//  Created by Khoi Nguyen on 10/18/19.
+//  Copyright © 2019 Khoi Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
